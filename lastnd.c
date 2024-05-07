e3a34a8ad088ad7e7f9f68db25f5efa9dc94839f
Promed Das <126240257+promed0@users.noreply.github.com>
2024-05-07 19:37:36 +0600

    lastnodedel.c

--- lastnd.c
+++ lastnd.c
@@ -1,42 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node {
     int data;
-    struct node *link;
+    struct node *next;
 };
-struct node *del_last(struct node *head){
-    if (head == NULL)
-    {
+
+struct node *del_last(struct node *head) {
+    if (head == NULL) {
         printf("List is empty\n");
-    }
-    else if (head ->link == NULL)   
-    {
+    } else if (head->next == NULL) {
         free(head);
         head = NULL;
-    }
-    else{
+    } else {
         struct node *temp = head;
-        struct node *temp2= head;
-        while (temp -> link != NULL)
-        {
+        struct node *temp2 = head;
+        while (temp->next != NULL) {
             temp2 = temp;
-            temp = temp->link;
-            temp2 ->link = NULL;
-            free(temp);
-            temp = NULL;
+            temp = temp->next;
         }
-        return head;
+        temp2->next = NULL;
+        free(temp);
     }
-};
-int main(){
+    return head;
+}
+
+int main() {
     struct node *head = NULL;
+    struct node *node1 = (struct node *)malloc(sizeof(struct node));
+    struct node *node2 = (struct node *)malloc(sizeof(struct node));
+    struct node *node3 = (struct node *)malloc(sizeof(struct node));
+
+    // Assign values
+    node1->data = 10;
+    node1->next = node2;
+
+    node2->data = 20;
+    node2->next = node3;
+
+    node3->data = 30;
+    node3->next = NULL;
+
+    head = node1;
+
+    // Delete the last node
     head = del_last(head);
-    struct node *ptr;
-    ptr = head;
-    while (ptr != NULL)
-    {
-        printf("%d ",ptr->data);
-        ptr = ptr->link;
+
+    // Print the remaining list
+    struct node *ptr = head;
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
     }
+    printf("\n");
     return 0;
 }
