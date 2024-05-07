79f7c983fb3adbdfdb128c667d2a561ab3744eaa
Promed Das <126240257+promed0@users.noreply.github.com>
2024-05-07 19:35:30 +0600

    Update anyposnd.c

--- anyposnd.c
+++ anyposnd.c
@@ -31,13 +31,30 @@ void del_pos(struct node **head, int position){
 };
 int main(){
     struct node *head;
+    struct node *node1 = (struct node *)malloc(sizeof(struct node));
+    struct node *node2 = (struct node *)malloc(sizeof(struct node));
+    struct node *node3 = (struct node *)malloc(sizeof(struct node));
+
+    // Assign values
+    node1->data = 10;
+    node1->link = node2;
+
+    node2->data = 20;
+    node2->link = node3;
+
+    node3->data = 30;
+    node3->link = NULL;
+
+    head = node1;
+
+    // Delete the last node
     int position = 2;
     del_pos(&head, position);
     struct node *ptr;
     ptr = head;
     while (ptr != NULL)
     {
-        printf("%d", ptr->data);
+        printf("%d ", ptr->data);
         ptr = ptr->link;
     }
     return 0;
