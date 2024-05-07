fdc2a05595c5a103b53a7a71211e964fb92202a8
Promed Das <126240257+promed0@users.noreply.github.com>
2024-05-07 19:36:08 +0600

    firstnd.c

--- firstnd.c
+++ firstnd.c
@@ -20,13 +20,31 @@ struct node *del_first(struct node *head)
     return head;
 };
 int main(){
-    struct node *head = NULL;
+   struct node *head = NULL;
+    struct node *node1 = (struct node *)malloc(sizeof(struct node));
+    struct node *node2 = (struct node *)malloc(sizeof(struct node));
+    struct node *node3 = (struct node *)malloc(sizeof(struct node));
+
+    // Assign values
+    node1->data = 10;
+    node1->link = node2;
+
+    node2->data = 20;
+    node2->link = node3;
+
+    node3->data = 30;
+    node3->link = NULL;
+
+    head = node1;
+
+    // Delete the last node
     head = del_first(head);
+
+    // Print the remaining list
     struct node *ptr = head;
-    while (ptr != NULL){    
-    {
-        printf("%d", ptr -> data);
-    }
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
     }
 return 0;
 }
